d34bb7bd23705f321d896652601ad693903a87c0
Ravi <ravi@gmail.com>
2022-08-11 15:08:35 +0530

    ec2 code

--- asg.tf
+++ asg.tf
@@ -3,7 +3,7 @@ resource "aws_autoscaling_group" "main" {
   max_size           = var.ASG_MAX
   min_size           = var.ASG_MIN
   vpc_zone_identifier = var.PRIVATE_SUBNET_ID
-  target_group_arns  = [aws_lb_target_group.target-group.arn]
+  target_group_arns  = aws_lb_target_group.target-group.arn
 
   launch_template {
     id      = aws_launch_template.launch_template.id
