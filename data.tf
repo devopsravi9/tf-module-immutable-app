33ca1730b05d7ec166102f6683375e769709007b
Ravi <ravi@gmail.com>
2022-08-11 15:01:05 +0530

    ec2 code

--- data.tf
+++ data.tf
@@ -26,3 +26,4 @@ data "aws_ec2_spot_price" "spot_price" {
 data "aws_subnet" "selected" {
   id = var.PRIVATE_SUBNET_ID[0]
 }
+
--- target_group.tf
+++ target_group.tf
@@ -1,17 +1,17 @@
-//resource "aws_lb_target_group" "target-group" {
-//  name     = "${var.COMPONENT}-${var.ENV}"
-//  port     = var.PORT
-//  protocol = "HTTP"
-//  vpc_id   = var.VPC_ID
-//
-//  health_check {
-//    enabled = true
-//    healthy_threshold = 2
-//    unhealthy_threshold = 2
-//    interval = 6
-//    path = "/health"
-//  }
-//}
+resource "aws_lb_target_group" "target-group" {
+  name     = "${var.COMPONENT}-${var.ENV}"
+  port     = var.PORT
+  protocol = "HTTP"
+  vpc_id   = var.VPC_ID
+
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 6
+    path = "/health"
+  }
+}
 
 //resource "aws_lb_target_group_attachment" "attach" {
 //  count            = var.INSTANCE_COUNT
